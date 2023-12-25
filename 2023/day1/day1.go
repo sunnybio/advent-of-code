5449c000c683a6e4b4f962d7c9a32e5c1e931780
sunnybio <wdeepeshsharma@gmail.com>
2023-12-25 20:26:51 +0530

    created advent of code repo

---
+++ 2023/day1/day1.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	// reading file for input
+	f, err := os.Open("input.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	s := bufio.NewScanner(f)
+	// constant array to check for the number in words
+	numArr := [10]string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+	fmt.Println(numArr)
+	var sum int32 = 0
+	// iterating lines to get the values
+	for s.Scan() {
+		var line string = s.Text()
+		var j int = len(line) - 1
+		var firstNum int8 = -1
+		var secondNum int8 = -1
+		for i := 0; i < len(line); i++ {
+
+			if isInteger(line[i]) && firstNum == -1 {
+				firstNum = int8(line[i])
+			}
+
+			if isInteger(line[j]) && secondNum == -1 {
+				secondNum = int8(line[j])
+			}
+
+			//fmt.Println(string(line[i]), string(line[j]))
+			j--
+		}
+		var comString string = string(firstNum) + string(secondNum)
+		if combinedNum, err := strconv.Atoi(comString); err == nil {
+			sum = sum + int32(combinedNum)
+		}
+	}
+	fmt.Println(sum)
+	f.Close()
+}
+
+func isInteger(asciiVal byte) bool {
+
+	if asciiVal > 47 && asciiVal < 58 {
+		return true
+	}
+	return false
+}
